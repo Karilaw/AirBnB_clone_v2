68e3e393a77e9ea49760bffba855a525398f981c
Karilaw <lawrencekariithi6@gmail.com>
2023-10-21 03:36:09 -0700

    committing task to test

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!' when the root URL is requested."""
+def hello():
+    """Route to display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB' when the '/hbnb' URL is requested."""
+    """Route to display 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Displays 'C when the '/c/<text>' URL is requested."""
-    return 'C {}'.format(text.replace('_', ' '))
+def c_text(text):
+    """
+    Route to display 'C ', followed by the variable.
+    Replace underscore _ symbols with a space.
+    """
+    return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """Displays 'Python ' followed by text>' URL is requested."""
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_text(text):
+    """
+    Route to display 'Python ', followed by the value.
+    Replace underscore _ symbols with a space.
+    The default value of text is 'is cool'.
+    """
+    return 'Python %s' % text.replace('_', ' ')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """Main method that starts the Flask web application"""
     app.run(host='0.0.0.0', port=5000)
